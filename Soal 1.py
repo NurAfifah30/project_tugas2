2514e6af1c69ccebd49376a30da95d7f39c23d6a
NurAfifah30 <115690241+NurAfifah30@users.noreply.github.com>
2022-11-03 09:37:11 +0700

    project_tugas2

---
+++ Soal 1.py
@@ -0,0 +1,16 @@
+# 1.Jelaskan type data dalam Bahasa dasar pemrograman dan beri contoh data :
+
+# - tipe data integer adalah tipa data yang digunakan apabila bertemi dengan bilangan bulat
+bilangan =10
+print(bilangan)
+
+# tipe data bolean adalah tipe data yang menhimpan dua nilai 
+x = True
+y = False
+
+print (x and y)
+print (x or y)
+
+# -tipe data string sering di sebut dengan tipe data teks
+data = "Muzanni 1994"
+print(data)
