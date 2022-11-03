80fc1a820da7c757a41869d752d7b1febb3c8553
NurAfifah30 <115690241+NurAfifah30@users.noreply.github.com>
2022-11-03 09:37:47 +0700

    project_tugas2

---
+++ soal 2.py
@@ -0,0 +1,13 @@
+# 2. buat contoh kode pemograman untuk membedakan tipe data di python
+
+# tipe data Dictionary
+data = {"nama":"Muzanni", "belajar":"bahasa Python"}
+print(data)
+
+# fungsi untuk melihat jenis tipe data
+data = 10;
+print(data, type(data))
+
+# tipe data List
+data = ["Muzanni", 1994]
+print(data)
